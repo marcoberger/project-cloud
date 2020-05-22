c6f428bcc076874162d1bcf466cc2cdf5859b611
Marco Berger <marco.berger@codecentric.de>
2020-05-22 14:36:25 +0200

    Remove unused variables

--- infrastructure/environments/dev/variables.tf
+++ infrastructure/environments/dev/variables.tf
@@ -2,19 +2,3 @@ variable "region" {
   description = "The dfault AWS Region"
   default     = "eu-central-1"
 }
-
-variable "environment" {
-  description = "The environment of the application. Can be development (dev), staging (stage) or production (prod)"
-  default     = "dev"
-}
-
-variable "team" {
-  description = "The name of the team"
-  default     = "jedis"
-}
-
-variable "project" {
-  description = "The name of the project"
-  default     = "project-cloud"
-}
-
